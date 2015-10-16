095a208ff6b8f152eddc4085131cd48819a7df6b
leo <leoxyznull@gmail.com>
2015-10-16 17:20:03 +0800

    first commit

---
+++ test/snmpdemoTest.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Administrator on 2015/10/16.
+ */
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class snmpdemoTest {
+    @Test
+    public void testAdd() {
+        assertEquals(5,5);
+    }
+
+    @Test
+    public void testSubcract(){
+        assertEquals(3,2);
+    }
+}
